ad804f5ac0a8aca9033fd79f0d44d89924222723
Nithi <nithyadevi386@gmail.com>
2021-04-19 20:36:44 +0530

    work is now changed

--- src/org/factorial/Factorial.java
+++ src/org/factorial/Factorial.java
@@ -1,6 +1,16 @@
 package org.factorial;
 
 public class Factorial {
+	private void methodOfNew() {
+		System.out.println("New Method is Created");
+
+	}
+	private void methodOfSecomd() {
+		System.out.println("Second Method is Created");
+
+
+	}
+	
 	public static void main(String[] args) {
 		
 int i,n=5, fact=1;
